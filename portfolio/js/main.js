9b1faaa6f6dbadd6f599ffab79295727f2b4b7cc
KakeruHoshi <kakeru1030@icloud.com>
2021-09-17 14:42:41 +0900

    update

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -76,3 +76,25 @@ $(function(){
     });
 
 });
+
+function popup() {
+    var popup = document.getElementById('js-popup');
+    if(!popup) return;
+  
+    var blackBg = document.getElementById('js-black-bg');
+    var closeBtn = document.getElementById('js-close-btn');
+    var showBtn = document.getElementById('js-show-popup');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    console.log(1);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+  }
+  popup();
+  
